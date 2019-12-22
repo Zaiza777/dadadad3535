54411b274ee3785c66a23d98e29b0e9e2c627a43
rise <rise@localhost.localdomain>
2019-12-22 20:10:03 +0300

    xDDD

--- ruby.rb
+++ ruby.rb
@@ -1,12 +1,12 @@
 require 'open-uri'
 require 'nokogiri'
 
-url =  'https://www.socks-proxy.net/'
-
+url = "https://www.socks-proxy.net/"
 html = open(url)
 xlaa = Nokogiri::HTML(html)
 
 doc = xlaa.css('.table-responsive tbody')
-
-shrek = doc.css('td')
-puts shrek.remove_class
+shrek = doc.css("td")
+shrek.each do |item|
+	puts item.text.gsub(/([0-9]){1,3}.([0-9]){1,3}.([0-9]){1,3}.([0-9]){1,3}/)
+end
